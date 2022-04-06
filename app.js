a9be263ca8aa6b1d39e11c1e03bce478147eb53c
chisatam <First12508@gmail.com>
2022-04-06 23:08:48 +0700

    update html fontend

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
-const express = require('express');
-const chalk = require('chalk');
-const debug = require('debug')('app');
-const morgan = require('morgan')
+const express = require("express");
+const chalk = require("chalk");
+const debug = require("debug")("app");
+const morgan = require("morgan");
+const path = require("path");
 const app = express();
 const port = 3000;
 
-app.use(morgan('combined'));
 
-app.get("/",(req,res)=>{
-    res.send('Hellow refirst tps org top');
-})
-app.listen(port,()=>{
+app.use(morgan("combined"));
+app.use(express.static(path.join(__dirname,"/public/")));
+
+app.get("/", (req, res) => {
+    res.send("Hellow refirst tps org top");
+});
+app.listen(port,path, () => {
     debug("Listening on port " + chalk.green(port));
-})
+    debug("Log phat ="+chalk.green(__dirname));
+});
